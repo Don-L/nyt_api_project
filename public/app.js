ac4691c461a34afd6f95fcdeee1996b069bc9963
Donald Lessells <donald.lessells@gmail.com>
2016-06-12 18:40:20 +0100

    got select working with api request

--- public/app.js
+++ public/app.js
@@ -14,11 +14,15 @@ window.onload = function() {
 
 
 
+
+
+
+
   // var trumpUrls = urlMaker("trump,+donald+j");
   // var clintonUrls = urlMaker("Clinton,+Hillary+Rodham");
 
-  var trumpNumbers = [];
-  var clintonNumbers = [];
+  // var trumpNumbers = [];
+  // var clintonNumbers = [];
 
   // for (url of trumpUrls) {
   //   var requestTrump = new XMLHttpRequest();
@@ -39,6 +43,49 @@ window.onload = function() {
         // console.log(articles).typeOf;
         // trump(trumpDocs);
       // }
+  }
+
+  var trumpNumber = 0;
+  var clintonNumber = 0;
+
+  var handleClick = function(event) {
+      event.preventDefault();
+      console.log('Woah, I got clicked!');
+      var monthSelect = document.getElementById('month');
+      var yearSelect = document.getElementById('year');
+      var monthAndYear = [yearSelect.value, monthSelect.value];
+      requestNumbers(monthAndYear[0], monthAndYear[1]);
+  }
+
+  requestNumbers = function(year, month) {
+    var requestTrump = new XMLHttpRequest();
+    var urlTrump = urlMaker("trump,+donald+j", month, year);
+    console.log(urlTrump);
+    requestTrump.open("GET", urlTrump);
+    requestTrump.send(null);
+    requestTrump.onload = function() {
+      if (requestTrump.status === 200) {
+        console.log('got the trump data');
+        var jsonString = requestTrump.responseText;
+        returnedObj = JSON.parse(jsonString);
+        console.log(returnedObj);
+        trumpNumber = returnedObj.response.meta.hits;
+      }
+    }
+    var requestClinton = new XMLHttpRequest();
+    var urlClinton = urlMaker("Clinton,+Hillary+Rodham", month, year);
+    requestClinton.open("GET", urlClinton);
+    requestClinton.send(null);
+    requestClinton.onload = function() {
+      if (requestClinton.status === 200) {
+        console.log('got the clinton data');
+        var jsonString = requestClinton.responseText;
+        returnedObj2 = JSON.parse(jsonString);
+        console.log(returnedObj2);
+        clintonNumber = returnedObj2.response.meta.hits;
+      }
+    }
+    console.log('trump ' + trumpNumber + 'clinton ' + clintonNumber);
   }
 
   // for (url of clintonUrls) {
@@ -86,14 +133,7 @@ window.onload = function() {
   // };
 // };
 // }
-var handleClick = function(event) {
-    event.preventDefault();
-    console.log('Woah, I got clicked!');
-    var monthSelect = document.getElementById('month');
-    var yearSelect = document.getElementById('year');
-    monthAndYear = [yearSelect, monthSelect];
-    requestData();
-}
+
 
 // var trump = function(docs) {
 //   displayHeadlines(docs, 'trump');
@@ -128,22 +168,24 @@ var monthEnd  = [
     ['12', '31'],
 ];
 
-// var dateRangeMaker = function(month, year) {
-//   var beginAndEndDates = [];
-//   for (pair of monthEnd) {
-//     if (pair[0] === month) {
-//       beginAndEndDates.push([year, month ,'01']);
-//       beginAndEndDates.push([year + month + pair[1]]);
-//     }
-//   };
-//   return beginAndEndDates;
-// };
+var dateRangeMaker = function(month, year) {
+  var beginAndEndDates = [];
+  for (pair of monthEnd) {
+    if (pair[0] === month) {
+      beginAndEndDates.push([year, month ,'01']);
+      beginAndEndDates.push([year, month, pair[1]]);
+    }
+  };
+  return beginAndEndDates;
+};
 
-// var urlMaker = function(queryString, month, year) {
-//   var datePairs = dateRangeMaker(month, year);
-//   var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&apikey=8940f70a78ca497f9f44e177685c1fdb&q=" + queryString + "&begin_date=" + datePairs[0][0] + datePairs[0][1] + datePairs[0][2] + "&end_date=" + datePairs[1][0] + datePairs[1][1] + datePairs[1][2];
-//   return url;
-// };
+var urlMaker = function(queryString, month, year) {
+  var datePairs = dateRangeMaker(month, year);
+  console.log(datePairs + ' DATEPairs')
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&apikey=8940f70a78ca497f9f44e177685c1fdb&q=" + queryString + "&begin_date=" + datePairs[0][0] + datePairs[0][1] + datePairs[0][2] + "&end_date=" + datePairs[1][0] + datePairs[1][1] + datePairs[1][2];
+  console.log(url + 'URL');
+  return url;
+};
 
 
 
