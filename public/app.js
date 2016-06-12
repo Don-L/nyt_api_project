548e49eb26dfb3820b548c692a85c47fe17bddcb
Donald Lessells <donald.lessells@gmail.com>
2016-06-12 17:36:05 +0100

    had to make major changes due to limits on requests from api

--- public/app.js
+++ public/app.js
@@ -15,105 +15,118 @@ window.onload = function() {
   var clintonNumbers = [];
 
   for (url of trumpUrls) {
-    requestTrump = new XMLHttpRequest();
-    requestTrump.open("GET", urlTrump);
+    var requestTrump = new XMLHttpRequest();
+    requestTrump.open("GET", url);
     requestTrump.send(null);
-    requestTrump.onload();
+    requestTrump.onload = function() {
+      if (requestTrump.status === 200) {
+        console.log('got the trump data');
+        var jsonString = requestTrump.responseText;
+        returnedObj = JSON.parse(jsonString);
+        console.log(returnedObj);
+        // var trumpDocs = returnedObj.response.docs;
+        // var trumpHits = returnedObj.response.meta.hits;
+        // console.log('trump hits ' + trumpHits);
+        // var dates = dateRangeMaker();
+        // console.log('dates ' + dates);
+        // console.log('dates ' + dates[0]);
+        // console.log(articles).typeOf;
+        // trump(trumpDocs);
+      }
   }
 
-  requestTrump.open("GET", urlTrump);
-  requestTrump.send(null);
+  // for (url of clintonUrls) {
+  //   var requestClinton = new XMLHttpRequest();
+  //   requestClinton.open("GET", url);
+  //   requestClinton.send(null);
+  //   requestClinton.onload();
+  // }
 
-  requestClinton.open("GET", urlClinton);
-  requestClinton.send(null);
+  // requestTrump.open("GET", urlTrump);
+  // requestTrump.send(null);
 
-  requestTrump.onload = function() {
-    if (requestTrump.status === 200) {
-      console.log('got the trump data');
-      var jsonString = requestTrump.responseText;
-      returnedObj = JSON.parse(jsonString);
-      console.log(returnedObj);
-      var trumpDocs = returnedObj.response.docs;
-      var trumpHits = returnedObj.response.meta.hits;
-      console.log('trump hits ' + trumpHits);
-      var dates = dateRangeMaker();
-      console.log('dates ' + dates);
-      console.log('dates ' + dates[0]);
+  // requestClinton.open("GET", urlClinton);
+  // requestClinton.send(null);
+
+  // requestTrump.onload = function() {
+  //   if (requestTrump.status === 200) {
+  //     console.log('got the trump data');
+  //     var jsonString = requestTrump.responseText;
+  //     returnedObj = JSON.parse(jsonString);
+  //     console.log(returnedObj);
+      // var trumpDocs = returnedObj.response.docs;
+      // var trumpHits = returnedObj.response.meta.hits;
+      // console.log('trump hits ' + trumpHits);
+      // var dates = dateRangeMaker();
+      // console.log('dates ' + dates);
+      // console.log('dates ' + dates[0]);
       // console.log(articles).typeOf;
-      trump(trumpDocs);
-    }
+      // trump(trumpDocs);
+    // }
   }
 
-  requestClinton.onload = function() {
-    if (requestClinton.status === 200) {
-      console.log('got the clinton data');
-      var jsonString = requestClinton.responseText;
-      returnedObj = JSON.parse(jsonString);
-      console.log(returnedObj);
-      var clintonDocs = returnedObj.response.docs;
-      // console.log(articles).typeOf;
-      clinton(clintonDocs);
-      var clintonUrls = urlMaker("Clinton,+Hillary+Rodham");
-      console.log(clintonUrls);
-    };
-  };
-};
+  // requestClinton.onload = function() {
+    // if (requestClinton.status === 200) {
+    //   console.log('got the clinton data');
+    //   var jsonString = requestClinton.responseText;
+    //   returnedObj = JSON.parse(jsonString);
+    //   console.log(returnedObj);
+    //   var clintonDocs = returnedObj.response.docs;
+    //   // console.log(articles).typeOf;
+    //   clinton(clintonDocs);
+    //   var clintonUrls = urlMaker("Clinton,+Hillary+Rodham");
+    //   console.log(clintonUrls);
+    // };
+  // };
+// };
+}
 
-var trump = function(docs) {
-  displayHeadlines(docs, 'trump');
-};
 
-var clinton = function(docs) {
-  displayHeadlines(docs, 'clinton');
-};
+// var trump = function(docs) {
+//   displayHeadlines(docs, 'trump');
+// };
 
-var displayHeadlines = function(docs, candidate) {
-  var list = document.getElementById(candidate);
-  for (doc of docs) {
-    var headline = document.createElement('li');
-    headline.innerText = doc.headline.main
-    list.appendChild(headline);
-  };
-};
+// var clinton = function(docs) {
+//   displayHeadlines(docs, 'clinton');
+// };
 
-var monthStartEnd  = [
+// var displayHeadlines = function(docs, candidate) {
+//   var list = document.getElementById(candidate);
+//   for (doc of docs) {
+//     var headline = document.createElement('li');
+//     headline.innerText = doc.headline.main
+//     list.appendChild(headline);
+//   };
+// };
 
-    ['01', '01', '31'],
-    ['02', '01', '28'],
-    ['03', '01', '31'],
-    ['04', '01', '30'],
-    ['05', '01', '31'],
-    ['06', '01', '30'],
-    ['07', '01', '31'],
-    ['08', '01', '31'],
-    ['09', '01', '30'],
-    ['10', '01', '31'],
-    ['11', '01', '30'],
-    ['12', '01', '31'],
+var monthEnd  = [
+
+    ['01', '31'],
+    ['02', '28'],
+    ['03', '31'],
+    ['04', '30'],
+    ['05', '31'],
+    ['06', '30'],
+    ['07', '31'],
+    ['08', '31'],
+    ['09', '30'],
+    ['10', '31'],
+    ['11', '30'],
+    ['12', '31'],
 ];
 
-var dateRangeMaker = function() {
+var dateRangeMaker = function(month, year) {
   var beginAndEndDates = [];
-  var i = 5;
-  var j = 0;
-  var k = 0;
-  while (i < 12) {
-    beginAndEndDates.push(['2014'+monthStartEnd[i][0]+monthStartEnd[i][1], '2014'+monthStartEnd[i][0]+monthStartEnd[i][2]]);
-    i++;
-  };
-  while (j < 12) {
-    beginAndEndDates.push(['2015'+monthStartEnd[j][0]+monthStartEnd[j][1], '2015'+monthStartEnd[j][0]+monthStartEnd[j][2]]);
-    j++;
-  };
-  while (k < 5) {
-    beginAndEndDates.push(['2016'+monthStartEnd[k][0]+monthStartEnd[k][1], '2016'+monthStartEnd[k][0]+monthStartEnd[k][2]]);
-    k++;
+  for (pair of monthEnd) {
+    if (pair[0] === month) {
+      beginAndEndDates.push([year + month + '01', year + month + pair[1]])
+    }
   };
   return beginAndEndDates;
 };
 
-var urlMaker = function(queryString) {
-  var datePairs = dateRangeMaker();
+var urlMaker = function(queryString, month, year) {
+  var datePairs = dateRangeMaker(month, year);
   var urls = [];
   for (pairs of datePairs) {
     urls.push("https://api.nytimes.com/svc/search/v2/articlesearch.json?&apikey=8940f70a78ca497f9f44e177685c1fdb&q=" + queryString + "&begin_date=" + pairs[0] + "&end_date=" + pairs[1]);
